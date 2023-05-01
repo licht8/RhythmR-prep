b722c9332de65f3e22cdcdfaabc0b367b0e8603a
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-01 23:49:52 +0200

    Update swap.sh

--- bin/scripts/swap.sh
+++ bin/scripts/swap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check the current swap memory
-swap=$(free | grep Swap | awk '{print $2}')
-echo "The current swap memory is $swap KB"
+swap=$(free -m | awk '/^Swap:/ {print $2}')
+echo "The current swap memory is $swap MB"
 
 # Ask the user if they want to add more swap memory
 read -p "Do you want to add more swap memory? [y/n] " answer
@@ -15,14 +15,14 @@ if [ "$answer" = "y" ]; then
   new_swap=$(expr $new_swap \* 1024)
 
   # Add the new swap memory
-  sudo fallocate -l $new_swap /swapfile
-  sudo chmod 600 /swapfile
-  sudo mkswap /swapfile
-  sudo swapon /swapfile
+  fallocate -l "${new_swap}M" /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
 
   # Confirm the new swap memory
-  new_swap=$(free | grep Swap | awk '{print $2}')
-  echo "The new swap memory is $new_swap KB"
+  new_swap=$(free -m | grep Swap | awk '{print $2}')
+  echo "The new swap memory is $new_swap MB"
 else
   echo "Exiting the script"
 fi

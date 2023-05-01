98a8dcdecc1a064240fb3e93377d4cbf9f6f2e76
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-01 23:34:01 +0200

    Update install.sh

--- bin/scripts/install.sh
+++ bin/scripts/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be run as root"
+  exit
+fi
+
 dnf update -y
-dnf install -y git curl zip unzip tar
+dnf install -y epel-release git curl tar mc net-tools iftop htop atop bzip2 traceroute gdisk lsof
+
+# Выключаем SELinux
+setenforce 0
+sestatus

3146e425bf994a6668bd9685b5ce5c6117ea4e10
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-01 20:37:53 +0200

    Create preinstall.sh

---
+++ preinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# upd
+dnf update -y
+
+# install & copy git
+cd ~/
+dnf install git -y
+git clone https://github.com/licht8/RythmR-prep.git
+
+cd RythmR-prep/bin/scripts/
+chmod +x install.sh
+./install

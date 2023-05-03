9a64e6e94db3ea336b396c88243b20933650efbb
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-03 04:58:47 +0200

    .

--- bin/functions/.load_function.sh
+++ bin/functions/.load_function.sh
@@ -2,9 +2,10 @@
 
 # --> Read the settings from the function file:
 
-# --> Загрузка функций с HELP
-	. /root/vdsetup.2/bin/.help/.load_help_func.sh
-
+# --> Load a function "search_string"
+	. /root/RhythmR-prep/bin/functions/search_string.sh
+# --> Load a function "run_as_root"
+	. /root/RhythmR-prep/bin/functions/run_as_root.sh
 
 
 # --> This function link checks if the script is running with superuser (root) rights in Linux.

75105cb039bbfc5dad3d1620728294cdcb48ca87
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-03 04:04:23 +0200

    .

---
+++ bin/functions/.load_function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# --> Read the settings from the function file:
+
+# --> Загрузка функций с HELP
+	. /root/vdsetup.2/bin/.help/.load_help_func.sh
+
+
+
+# --> This function link checks if the script is running with superuser (root) rights in Linux.
+#	. /root/vdsetup.2/bin/functions/run_as_root.sh
